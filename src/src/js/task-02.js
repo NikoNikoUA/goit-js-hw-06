63944aacbaddcb3001c8d67dc3ca98ff037027e2
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-07-10 21:09:27 +0300

    Update task-02.js

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = ingredients.forEach(function (ingredient) {
+
+  const ingredientsListEl = document.createElement('li');
+  ingredientsListEl.classList.add('item');
+  ingredientsListEl.textContent = ingredient;
+  
+  const totalList = document.querySelector('#ingredients');
+  totalList.append(ingredientsListEl);
+ 
+});
+
+
+
