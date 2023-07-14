088d74f51741cf3be3bf8b3949254a94ae9ba4e1
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-07-14 17:48:29 +0300

    Changes to task 5

--- src/src/js/task-05.js
+++ src/src/js/task-05.js
@@ -1,7 +1,7 @@
-const inputName = document.querySelector("#name-input");
+const inputText = document.querySelector("#name-input");
 const headerName = document.querySelector("#name-output");
 
-inputName.addEventListener("input", onInputChange);
+inputText.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
   if (event.currentTarget.value) {
