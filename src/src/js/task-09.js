ec70346f04b0bdf4548cc7902c38c7764950e1fa
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-07-17 22:41:21 +0300

    Task 9 completed

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,5 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+const btnEl = document.querySelector(".change-color");
+const colorIdentifyer = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btnEl.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+  colorIdentifyer.textContent = body.style.backgroundColor;
 }
+
+// ======== function for obtaining random hexcolor ============
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
