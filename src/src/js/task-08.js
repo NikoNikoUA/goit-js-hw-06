8888786e8d22e3d4fbe56f095bbea523f22042db
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-07-18 23:24:57 +0300

    Task 8 completed

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,55 @@
+// ===================== OPTION 1 =====================
 
+const loginForm = document.querySelector(".login-form");
+const inputEl = document.querySelectorAll("input");
+
+loginForm.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    console.log(alert("Please, fill in all fields!!!"));
+  } else {
+    loginForm.reset();
+  }
+
+  const inputDataCollection = {
+    email,
+    password,
+  };
+
+  console.log(inputDataCollection);
+}
+
+// ===================== OPTION 2 =====================
+
+// const loginForm = document.querySelector(".login-form");
+// const inputEl = document.querySelectorAll("input");
+
+// loginForm.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.currentTarget);
+
+//   console.log(formData);
+
+//   formData.forEach((name, value) => {
+//     console.log(value);
+//     console.log(name);
+//   });
+
+//   const email = event.currentTarget.elements.email.value;
+//   const password = event.currentTarget.elements.password.value;
+
+//   if (email === "" || password === "") {
+//     console.log(alert("Please, fill in all fields!!!"));
+//   } else {
+//     loginForm.reset();
+//   }
+// }
